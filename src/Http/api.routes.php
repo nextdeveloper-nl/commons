6f5be007a78a46005890040ec6f6ac604ac90fa0
Harun Baris Bulut <baris.bulut@plusclouds.com>
2023-12-03 14:29:15 +0300

    Transformer fixed

--- src/Http/api.routes.php
+++ src/Http/api.routes.php
@@ -224,26 +224,6 @@ Route::prefix('commons')->group(
 
         // EDIT AFTER HERE - WARNING: ABOVE THIS LINE MAY BE REGENERATED AND YOU MAY LOSE CODE
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        \n\n
-
         Route::get('/tags/object', 'Taggables\ObjectTagsController@index');
         Route::post('/tags/object', 'Taggables\ObjectTagsController@store');
     }
