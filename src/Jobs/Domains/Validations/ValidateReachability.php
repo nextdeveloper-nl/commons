24455b45c53656d978d65c963610997e7fecd3f5
hen8y <dheryhenry@gmail.com>
2024-02-07 16:40:43 +0100

    error with test

--- src/Jobs/Domains/Validations/ValidateReachability.php
+++ src/Jobs/Domains/Validations/ValidateReachability.php
@@ -41,45 +41,36 @@ class ValidateReachability extends AbstractAction
 
 
 
-
-
-
     /**
      * Checks if the domain is reachable and registered
      */
 
     public function handle()
     {
-        Log::info("Validating domain action started for: ". $this->model->name);
 
         //  Application starts here
-        // $this->setProgress(0, 'Validating domain action started');
-
+        $this->setProgress(0, 'Validating domain action started');
         if($this->checkDomainIsReachable()) {
             $validationData['is_reachable'] = true;
             $this->validatable->update([
                 'validation_data'   =>  $validationData,
             ]);
 
-            // $this->setProgress(66, 'We validate domain if it is reachable');
-            Log::info("We validate domain that it is reachable");
+            $this->setProgress(66, 'We validate domain if it is reachable');
         } else {
             $validationData['is_reachable'] = false;
             $this->validatable->update([
                 'validation_data'   =>  $validationData,
             ]);
-            // $this->setProgress(66, 'We cannot validate domain if it is reachable');
-            Log::info("We cannot validate domain if it is reachable");
+            $this->setProgress(66, 'We cannot validate domain if it is reachable');
         }
-
         if ($this->checkDomainIsRegistered()){
 
-            // $this->setProgress(33, 'We validate domain if it is registered');
+            $this->setProgress(33, 'We validate domain if it is registered');
             $validationData['is_registered'] = true;
             $this->validatable->update([
                 'validation_data'   =>  $validationData,
             ]);
-            Log::info("We validate domain if it is registered");
 
         } else {
 
@@ -88,21 +79,17 @@ class ValidateReachability extends AbstractAction
                 'validation_data'   =>  $validationData,
             ]);
 
-            // $this->setProgress(33, 'We cannot validate domain if it is registered');
-            Log::info("We validate domain if it is registered");
+            $this->setProgress(33, 'We cannot validate domain if it is registered');
         }
-
 
         $this->validatable->refresh();
 
         if($validationData['is_registered'] && $validationData['is_reachable']){
-            Log::info("isRegistered");
             $this->model->is_reachable = true;
             $this->model->save();
         }
 
-        // $this->setProgress(100, 'Validating domain action completed');
-        Log::info("Validating domain action completed");
+        $this->setProgress(100, 'Validating domain action completed');
 
         //  Application ends here
     }
@@ -115,13 +102,10 @@ class ValidateReachability extends AbstractAction
      */
     private function checkDomainIsRegistered() : bool {
         $whois = Factory::get()->createWhois();
-        $info = $whois->loadDomainInfo("loupp.io");
+        $url = $this->model->name;
+        $info = $whois->loadDomainInfo($url);
         if($info){
-            if(date('Y-m-d') > date("Y-m-d", $info->expirationDate)){
-                return false;
-            }else{
-                return true;
-            }
+            return true;
         }else{
             return false;
         }
@@ -152,5 +136,9 @@ class ValidateReachability extends AbstractAction
             // Domain is not reachable
             return false;
         }
+    }
+
+    public function fail($exception = null){
+        dd($exception->getMessage());
     }
 }
