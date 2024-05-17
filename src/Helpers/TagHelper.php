093fbac96124ffe060d304edf3c308e9d89786d4
IBRAHIM KHALIL ATTEIB YACOUB <i.khalilatteib@gmail.com>
2024-05-17 14:01:45 +0300

    Feature: tag and untag method added in TagHelper class

--- src/Helpers/TagHelper.php
+++ src/Helpers/TagHelper.php
@@ -2,9 +2,14 @@
 
 namespace NextDeveloper\Commons\Helpers;
 
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
+use InvalidArgumentException;
 use NextDeveloper\Commons\Database\Models\Tags;
+use NextDeveloper\Commons\Exceptions\CannotCreateModelException;
+use NextDeveloper\Commons\Exceptions\ModelNotFoundException;
 use NextDeveloper\Commons\Services\TagsService;
+use NextDeveloper\IAM\Database\Scopes\AuthorizationScope;
 
 class TagHelper
 {
@@ -39,4 +44,83 @@ class TagHelper
 
         return $obj;
     }
+
+    /**
+     * Clean and format the tags array
+     *
+     * @param array $tags
+     * @return array
+     */
+    private static function cleanTags(array $tags): array
+    {
+        $cleanedTags = array_map(function ($tag) {
+            return trim($tag, " \t\n\r\0\x0B\"");
+        }, $tags);
+
+        $uniqueTags = array_unique($cleanedTags);
+
+        return array_filter($uniqueTags, function ($tag) {
+            return !empty($tag);
+        });
+    }
+
+    /**
+     * Tag an object with a tag
+     *
+     * @param object $object
+     * @param string $tag
+     *
+     * @return void
+     * @throws InvalidArgumentException
+     * @throws ModelNotFoundException
+     * @throws CannotCreateModelException
+     */
+    public static function tag(object $object, string $tag): void
+    {
+
+        $tagObj = Tags::withoutGlobalScope(AuthorizationScope::class)
+            ->where('name', 'like', $tag)
+            ->first();
+
+        if (!$tagObj) {
+            $tagObj = TagsService::create([
+                'name'        => $tag,
+                'description' => '',
+                'slug'        => Str::slug($tag),
+            ]);
+        }
+
+        if (!$tagObj) {
+            throw new CannotCreateModelException('Cannot create the tag you are looking for.');
+        }
+
+        $objectTags = $object->tags;
+
+        if (!in_array($tagObj->name, $objectTags)) {
+            $objectTags[] = $tagObj->name;
+            $object->tags = self::cleanTags($objectTags);
+            $object->save();
+        }
+    }
+
+    /**
+     * Untag an object with a tag
+     *
+     * @param object $object
+     * @param string $tag
+     * @return void
+     * @throws InvalidArgumentException
+     */
+    public static function untag(object $object, string $tag): void
+    {
+        $objectTags = self::cleanTags($object->tags);
+
+        if (($key = array_search($tag, $objectTags))) {
+            unset($objectTags[$key]);
+            $object->tags = self::cleanTags($objectTags);
+            $object->save();
+        }
+
+    }
+
 }
