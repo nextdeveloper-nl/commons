10611d0f8105f94d14a87eddec90f9ac2cc9af80
Harun Baris Bulut <baris.bulut@plusclouds.com>
2025-02-08 16:19:40 +0300

    Pagination is fixed

--- src/Services/AbstractServices/AbstractCommentsService.php
+++ src/Services/AbstractServices/AbstractCommentsService.php
@@ -65,10 +65,10 @@ class AbstractCommentsService
             //  We are using this because we have been experiencing huge security problem when we use the paginate method.
             //  The reason was, when the pagination method was using, somehow paginate was discarding all the filters.
             return new \Illuminate\Pagination\LengthAwarePaginator(
-                $model->skip(($request->get('page', 1) - 1) * $perPage)->take($perPage)->get(),
+                $model->skip(((array_key_exists('page', $params) ? $params['page'] : 1) - 1) * $perPage)->take($perPage)->get(),
                 $model->count(),
                 $perPage,
-                $request->get('page', 1)
+                request()->get('page', 1)
             );
         }
 
@@ -181,7 +181,7 @@ class AbstractCommentsService
                 $data['iam_user_id']
             );
         }
-                    
+
         if(!array_key_exists('iam_user_id', $data)) {
             $data['iam_user_id']    = UserHelper::me()->id;
         }
@@ -191,7 +191,7 @@ class AbstractCommentsService
                 $data['parent_id']
             );
         }
-                        
+
         try {
             $model = Comments::create($data);
         } catch(\Exception $e) {
@@ -249,7 +249,7 @@ class AbstractCommentsService
                 $data['parent_id']
             );
         }
-    
+
         try {
             $isUpdated = $model->update($data);
             $model = $model->fresh();
--- src/Services/AbstractServices/AbstractPhoneNumbersService.php
+++ src/Services/AbstractServices/AbstractPhoneNumbersService.php
@@ -65,10 +65,10 @@ class AbstractPhoneNumbersService
             //  We are using this because we have been experiencing huge security problem when we use the paginate method.
             //  The reason was, when the pagination method was using, somehow paginate was discarding all the filters.
             return new \Illuminate\Pagination\LengthAwarePaginator(
-                $model->skip(($request->get('page', 1) - 1) * $perPage)->take($perPage)->get(),
+                $model->skip(((array_key_exists('page', $params) ? $params['page'] : 1) - 1) * $perPage)->take($perPage)->get(),
                 $model->count(),
                 $perPage,
-                $request->get('page', 1)
+                request()->get('page', 1)
             );
         }
 
@@ -181,7 +181,7 @@ class AbstractPhoneNumbersService
                 $data['common_country_id']
             );
         }
-                        
+
         try {
             $model = PhoneNumbers::create($data);
         } catch(\Exception $e) {
@@ -233,7 +233,7 @@ class AbstractPhoneNumbersService
                 $data['common_country_id']
             );
         }
-    
+
         try {
             $isUpdated = $model->update($data);
             $model = $model->fresh();
