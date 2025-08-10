663c31bc4f09e8e3a6e77de02db42a069cf19fd4
Harun Baris Bulut <baris.bulut@plusclouds.com>
2025-08-10 22:31:07 +0300

    Categories service updated

--- src/Services/CategoriesService.php
+++ src/Services/CategoriesService.php
@@ -2,7 +2,9 @@
 
 namespace NextDeveloper\Commons\Services;
 
+use NextDeveloper\Commons\Database\Models\Categories;
 use NextDeveloper\Commons\Services\AbstractServices\AbstractCategoriesService;
+use NextDeveloper\IAM\Database\Scopes\AuthorizationScope;
 
 /**
  * This class is responsible from managing the data for Categories
@@ -15,4 +17,16 @@ class CategoriesService extends AbstractCategoriesService
 {
 
     // EDIT AFTER HERE - WARNING: ABOVE THIS LINE MAY BE REGENERATED AND YOU MAY LOSE CODE
+
+    public static function getPublicCategories($domainUuid) {
+        $domain = \NextDeveloper\Commons\Database\Models\Domains::withoutGlobalScope(AuthorizationScope::class)
+            ->where('uuid', $domainUuid)
+            ->first();
+
+        $categories = Categories::withoutGlobalScope(AuthorizationScope::class)
+            ->where('common_domain_id', $domain->id)
+            ->get();
+
+        return $categories;
+    }
 }
