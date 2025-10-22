4ebb674fd85408b677e5922f90ce74a73a3747e5
Harun Baris Bulut <baris.bulut@plusclouds.com>
2025-10-23 01:29:19 +0300

    Default stateObject is now this->model

--- src/Actions/AbstractAction.php
+++ src/Actions/AbstractAction.php
@@ -252,6 +252,9 @@ class AbstractAction implements ShouldQueue
 
     public function getRunningAction() : ?Actions
     {
+        if(!$this->stateObject)
+            $this->stateObject = $this->model;
+
         $runningAction = StateHelper::getRunningAction($this->stateObject, $this->action);
 
         if($runningAction) {
@@ -263,6 +266,9 @@ class AbstractAction implements ShouldQueue
 
     public function getCheckpoint() : int
     {
+        if(!$this->stateObject)
+            $this->stateObject = $this->model;
+
         $runningState = StateHelper::getRunningAction($this->stateObject, $this->action);
 
         if($runningState) {
