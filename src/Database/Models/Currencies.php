ccbffd8455b029893dd67f08cbe81145480203cb
Harun Baris Bulut <baris.bulut@plusclouds.com>
2026-01-03 18:20:35 +0300

    Get addresses updated for CRM

--- src/Database/Models/Currencies.php
+++ src/Database/Models/Currencies.php
@@ -134,6 +134,16 @@ class Currencies extends Model
         return $this->hasMany(\NextDeveloper\CRM\Database\Models\QuoteItems::class);
     }
 
+    public function invoiceItems() : \Illuminate\Database\Eloquent\Relations\HasMany
+    {
+        return $this->hasMany(\NextDeveloper\Accounting\Database\Models\InvoiceItems::class);
+    }
+
+    public function transactions() : \Illuminate\Database\Eloquent\Relations\HasMany
+    {
+        return $this->hasMany(\NextDeveloper\Accounting\Database\Models\Transactions::class);
+    }
+
     // EDIT AFTER HERE - WARNING: ABOVE THIS LINE MAY BE REGENERATED AND YOU MAY LOSE CODE\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n
 
 
--- src/Http/Transformers/AbstractTransformers/AbstractAddressesTransformer.php
+++ src/Http/Transformers/AbstractTransformers/AbstractAddressesTransformer.php
@@ -53,28 +53,28 @@ class AbstractAddressesTransformer extends AbstractTransformer
      */
     public function transform(Addresses $model)
     {
-                                                $commonCountryId = \NextDeveloper\Commons\Database\Models\Countries::where('id', $model->common_country_id)->first();
-                                                            $iamAccountId = \NextDeveloper\IAM\Database\Models\Accounts::where('id', $model->iam_account_id)->first();
-                        
+        $commonCountryId = \NextDeveloper\Commons\Database\Models\Countries::where('id', $model->common_country_id)->first();
+        $iamAccountId = \NextDeveloper\IAM\Database\Models\Accounts::where('id', $model->iam_account_id)->first();
+
         return $this->buildPayload(
             [
-            'id'  =>  $model->uuid,
-            'object_id'  =>  $model->object_id,
-            'object_type'  =>  $model->object_type,
-            'name'  =>  $model->name,
-            'line1'  =>  $model->line1,
-            'line2'  =>  $model->line2,
-            'city'  =>  $model->city,
-            'state'  =>  $model->state,
-            'state_code'  =>  $model->state_code,
-            'postcode'  =>  $model->postcode,
-            'is_invoice_address'  =>  $model->is_invoice_address,
-            'common_country_id'  =>  $commonCountryId ? $commonCountryId->uuid : null,
-            'email_address'  =>  $model->email_address,
-            'iam_account_id'  =>  $iamAccountId ? $iamAccountId->uuid : null,
-            'created_at'  =>  $model->created_at,
-            'updated_at'  =>  $model->updated_at,
-            'deleted_at'  =>  $model->deleted_at,
+                'id' => $model->uuid,
+                'object_id' => $model->object_id,
+                'object_type' => $model->object_type,
+                'name' => $model->name,
+                'line1' => $model->line1,
+                'line2' => $model->line2,
+                'city' => $model->city,
+                'state' => $model->state,
+                'state_code' => $model->state_code,
+                'postcode' => $model->postcode,
+                'is_invoice_address' => $model->is_invoice_address,
+                'common_country_id' => $commonCountryId ? $commonCountryId->uuid : null,
+                'email_address' => $model->email_address,
+                'iam_account_id' => $iamAccountId ? $iamAccountId->uuid : null,
+                'created_at' => $model->created_at,
+                'updated_at' => $model->updated_at,
+                'deleted_at' => $model->deleted_at,
             ]
         );
     }
@@ -163,7 +163,6 @@ class AbstractAddressesTransformer extends AbstractTransformer
         return $this->collection($addresses, new AddressesTransformer());
     }
     // EDIT AFTER HERE - WARNING: ABOVE THIS LINE MAY BE REGENERATED AND YOU MAY LOSE CODE\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n
-
 
 
 }
