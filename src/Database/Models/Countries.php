0c73ccf5f3df25c7c1b0a7a3590f49af75d4db07
Harun Baris Bulut <baris.bulut@plusclouds.com>
2026-03-30 22:26:28 +0300

    IntegerArray object type added

--- src/Database/Models/Countries.php
+++ src/Database/Models/Countries.php
@@ -177,6 +177,21 @@ class Countries extends Model
         return $this->hasMany(\NextDeveloper\Accounting\Database\Models\PaymentGateways::class);
     }
 
+    public function accounts() : \Illuminate\Database\Eloquent\Relations\HasMany
+    {
+        return $this->hasMany(\NextDeveloper\IAM\Database\Models\Accounts::class);
+    }
+
+    public function users() : \Illuminate\Database\Eloquent\Relations\HasMany
+    {
+        return $this->hasMany(\NextDeveloper\IAM\Database\Models\Users::class);
+    }
+
+    public function accountTypes() : \Illuminate\Database\Eloquent\Relations\HasMany
+    {
+        return $this->hasMany(\NextDeveloper\IAM\Database\Models\AccountTypes::class);
+    }
+
     // EDIT AFTER HERE - WARNING: ABOVE THIS LINE MAY BE REGENERATED AND YOU MAY LOSE CODE\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n
 
     public function domains() : \Illuminate\Database\Eloquent\Relations\HasMany
